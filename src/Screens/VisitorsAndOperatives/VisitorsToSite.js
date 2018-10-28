6a2c46f80b1cac17b0c54869f51bad1257a0db97
adnankhandev <adnanak556@gmail.com>
2018-10-29 04:38:01 +0500

    dynamic visitors

--- src/Screens/VisitorsAndOperatives/VisitorsToSite.js
+++ src/Screens/VisitorsAndOperatives/VisitorsToSite.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 /* Presentational */
 import { View, Button } from 'react-native';
 
-import { Container, Content, Icon, Picker, Form } from "native-base";
+import { Container, Content, Icon, Picker, Form, List, ListItem, Text, Left, Right } from "native-base";
 import Selector from '../../Components/Selector/Selector';
 
 export default class VisitorsToSite extends Component {
@@ -19,30 +19,51 @@ export default class VisitorsToSite extends Component {
         title: null,
         quantity: null,
       ],
-      visitors: null,
+      addedVisitors: null,
     }
   }
 
-  initializeVisitors(){
-    const visitors = this.state.items.map((item) => {
+  onAdd(){
+    const items = this.state.items;
+    const addedVisitors = items.map((item) => {
       return(
-        
-      )
+        <ListItem>
+          <Left><Text>{item.name}</Text></Left>
+          <Right><Text>{item.quantity}</Text></Right>
+        <ListItem/>
+        )
     })
+    this.setState({addedVisitors})
   }
 
-  addVisitor(){
+  const currentTitle = null;
+  const currentQuantity = null;
 
+  let index = 0;
+    const data = 
+        [
+          {label: 'dummy data 1', value: '1', key: index++},
+          {label: 'dummy data 2', value: '2', key: index++},
+          {label: 'dummy data 3', value: '3', key: index++},
+        ]
+
+  onClick(a, b){
+    alert('a: ', a)
+    alert('b: ', b)
   }
 
-  mapPickerItems(){
-
-  }
 
   render() {
     return (
       <Container>
 	      <Content>
+          <Form>
+            <List>
+              {this.state.addedVisitors}
+            </List>
+          </Form>
+          <Selector items={data} onClick={onClick}/>
+          <Selector items={data} onClick={onClick}/>
 	      	<Button onPress={()=>{}} title="Click to add visitors to site"/>
       	</Content>
       </Container>
